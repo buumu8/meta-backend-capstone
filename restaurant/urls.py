f331dfe9cd98404e6ddd25f9f1309006a834c411
Jettapol Tuetrakul <buumu8@gmail.com>
2024-11-29 10:24:28 +0700

    update

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
-    path('book/', views.book, name="book"),
-    path('reservations/', views.reservations, name="reservations"),
-    path('menu/', views.menu, name="menu"),
-    path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),  
-    path('bookings', views.bookings, name='bookings'), 
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("book/", views.book, name="book"),
+    path("reservations/", views.reservations, name="reservations"),
+    path("menu/", views.menu, name="menu"),
+    path("menu_item/<int:pk>/", views.display_menu_item, name="menu_item"),
+    path("bookings", views.bookings, name="bookings"),
+    path("api-token-auth/", obtain_auth_token),
 ]
