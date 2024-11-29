3bd7d2d100025801fa03cc416d67692b1ecccfa0
Jettapol Tuetrakul <buumu8@gmail.com>
2024-11-29 10:27:06 +0700

    update

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -13,10 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('restaurant.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("restaurant.urls")),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_view"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
